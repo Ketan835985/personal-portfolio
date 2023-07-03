debf4c41b6b48b24182c644e1bf6e02ec5752921
Ketan Gupta <guptaketan6375@gmail.com>
2023-07-03 14:00:18 +0530

    backend Done

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const mongoose = require("mongoose");
+const {}= require("dotenv").config();
 
 // server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
+mongoose.connect("mongodb+srv://Ketan_technetium_functionUp:iDikLHzqHJQQP656@clusterketantechnetium.pexlgni.mongodb.net/PortFolio", 
+{useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
@@ -15,8 +21,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   },
 });
 
@@ -35,7 +41,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: process.env.EMAIL_USER,
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
@@ -46,6 +52,13 @@ router.post("/contact", (req, res) => {
     if (error) {
       res.json(error);
     } else {
+      mongoose.model.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phone: req.body.phone,
+        message: req.body.message
+      })
       res.json({ code: 200, status: "Message Sent" });
     }
   });
